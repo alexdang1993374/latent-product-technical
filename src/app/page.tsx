02ca07523b1fa2064623ef759057efb056baf558
Alexander Dang <alexdang1993374@gmail.com>
2023-06-23 21:51:39 -0700

    fix: responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,13 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <nav className="w-full flex flex-col md:flex-row items-center fixed top-0 z-50 px-24 py-5 gap-1 bg-black">
         <div className="w-20 h-10 flex items-center">
-          <Image src={latentLogo} alt="Latent Logo" className="object-cover" />
+          <Image
+            src={latentLogo}
+            alt="Latent Logo"
+            width={73}
+            height={14}
+            className="object-cover"
+          />
         </div>
 
         <div className="w-full flex justify-center">
@@ -17,7 +23,7 @@ export default function Home() {
         </div>
       </nav>
 
-      <div className="mt-10 md:mt-5 w-full flex flex-col text-center">
+      <div className="mt-10 md:mt-5 w-screen md:w-full h-[50vh] text-center px-4 md:px-0">
         <MedicationList />
       </div>
     </main>
--- src/components/MedicationList.tsx
+++ src/components/MedicationList.tsx
@@ -2,10 +2,13 @@
 
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { BounceLoader } from "react-spinners";
+import Image from "next/image";
 
 import useDrugSelector from "@/hooks/useDrugSelector";
 import { IMedicationResult, TMedication } from "@/types";
 import Medication from "./Medication";
+import latentLogo from "../../public/latent.svg";
 
 const MedicationList = () => {
   const drugSelector = useDrugSelector();
@@ -28,12 +31,35 @@ const MedicationList = () => {
     }
   }, [drugSelector]);
 
-  if (!medicationData) {
-    return;
+  if (medicationData.length === 0 && !loading) {
+    return (
+      <div className="w-full h-full flex justify-center items-center flex-col gap-16">
+        <div>
+          <Image
+            src={latentLogo}
+            alt="Latent Logo"
+            width={334}
+            height={64}
+            className="object-cover"
+          />
+        </div>
+
+        <div>
+          <p className="text-xl">
+            Building Medical Language Models to automate hospital operations,
+            starting with insurance authorizations
+          </p>
+        </div>
+      </div>
+    );
   }
 
   if (loading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="w-full flex justify-center">
+        <BounceLoader color="#ffffff" speedMultiplier={2} />
+      </div>
+    );
   }
 
   return (
