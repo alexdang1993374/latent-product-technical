a8fbe37982a15f53ac5c7f68a88f08d70fecf560
Alexander Dang <alexdang1993374@gmail.com>
2023-06-24 17:47:31 -0700

    fix: nav

--- src/components/LogoLink.tsx
+++ src/components/LogoLink.tsx
@@ -1,20 +1,37 @@
 "use client";
 
 import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
 
 import latentLogo from "../../public/latent.svg";
 import AccountIcon from "./AccountIcon";
 
 const LogoLink = () => {
+  const router = useRouter();
+  const pathName = usePathname();
+
+  const goHome = () => {
+    if (pathName !== "/account") {
+      return;
+    } else {
+      router.push("/");
+    }
+  };
+
   return (
     <div className="w-full md:w-20 h-10 flex items-center justify-between">
-      <Image
-        src={latentLogo}
-        alt="Latent Logo"
-        width={73}
-        height={14}
-        className="object-cover"
-      />
+      <div
+        className={pathName === "/account" ? "cursor-pointer" : ""}
+        onClick={goHome}
+      >
+        <Image
+          src={latentLogo}
+          alt="Latent Logo"
+          width={73}
+          height={14}
+          className="object-cover"
+        />
+      </div>
 
       <div className="flex md:hidden justify-end">
         <AccountIcon />
